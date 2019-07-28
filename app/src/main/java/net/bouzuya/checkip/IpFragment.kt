5591a43b3d27acb598a68a3381e724655bc94504
bouzuya <m@bouzuya.net>
2019-07-28 13:39:23 +0900

    add IpFragment

---
+++ app/src/main/java/net/bouzuya/checkip/IpFragment.kt
@@ -0,0 +1,31 @@
+package net.bouzuya.checkip
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import net.bouzuya.checkip.databinding.IpFragmentBinding
+
+
+class IpFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = IpFragment()
+    }
+
+    private val viewModel: IpViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return IpFragmentBinding.inflate(
+            inflater, container, false
+        ).also { binding ->
+            binding.lifecycleOwner = this
+            binding.viewModel = viewModel
+        }.root
+    }
+}
---
+++ app/src/main/java/net/bouzuya/checkip/IpViewModel.kt
@@ -0,0 +1,7 @@
+package net.bouzuya.checkip
+
+import androidx.lifecycle.ViewModel
+
+class IpViewModel : ViewModel() {
+    val name = "IP"
+}
