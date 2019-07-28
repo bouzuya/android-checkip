a07b057717ac7e76d9003ee522c519df51a2082d
bouzuya <m@bouzuya.net>
2019-07-28 13:57:11 +0900

    add home -> ip navigation

--- app/src/main/java/net/bouzuya/checkip/HomeFragment.kt
+++ app/src/main/java/net/bouzuya/checkip/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import net.bouzuya.checkip.databinding.HomeFragmentBinding
 
 
@@ -26,6 +27,11 @@ class HomeFragment : Fragment() {
         ).also { binding ->
             binding.lifecycleOwner = this
             binding.viewModel = viewModel
+
+            binding.homeIpButton.setOnClickListener {
+                val action = HomeFragmentDirections.actionHomeFragmentToIpFragment()
+                findNavController().navigate(action)
+            }
         }.root
     }
 }
